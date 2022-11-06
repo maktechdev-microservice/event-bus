986cb760fbba94d57c7753dc86f286e726a0b776
maktechdev <39600404+maktechdev@users.noreply.github.com>
2022-11-06 11:22:19 +0100

    Adds multiple changes

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-import express from "express"
-import axios from "axios"
-import BodyParser from "body-parser"
+import express from "express";
+import axios from "axios";
+import BodyParser from "body-parser";
 
-const app = express()
-app.use(BodyParser.json())
+const app = express();
+app.use(BodyParser.json());
 
-const port = 4005
+const port = 4005;
 
-const events = []
+const events = [];
 
 app.get("/events", (req, resp) => {
+  resp.send("<h1>Hello from Event Bus</h1>");
+});
 
-    resp.send("<h1>Hello from Event Bus</h1>")
-})
+app.post("/events", async (req, res) => {
+  const event = req.body;
+  console.log(`Event: ${event.type}`);
+  events.push(event);
+  const url = (ip) => `http://${ip}/events`;
+  const receivingIPs = [
+    "posts-clusterip-srv:4000",
+    "comments-clusterip-srv:4001",
+    "query-clusterip-srv:4002",
+    "moderator-clusterip-srv:4003",
+  ];
+  // 'moderator-clusterip-srv'
 
-app.post("/events",  async (req, res) => {
-    const event = req.body
-    console.log(`Event: ${event.type}`)
-    events.push(event)
-    const url = (ip) => `http://${ip}/events`
-    const receivingIPs = ['posts-clusterip-srv:4000', 'comments-clusterip-srv:4001', 'query-clusterip-srv:4002', 'moderator-clusterip-srv:4003']
+  receivingIPs.map(async (d) => {
+    await axios.post(url(d), event).catch((err) => {
+      console.log(`Event Bus reports: ${err} to post to ${d}`);
+    });
+  });
+  res.send({ status: "OK" });
+});
 
-    receivingIPs.map(async (d) => {
-        await axios.post(url(d), event).catch(err => {
-            console.log(`Event Bus reports: ${err} to post to ${d}`)
-        }
-        )
-    })
-    res.send({status: 'OK'})
-})
-
-app.listen(port, () => console.log(`Event Bus service listening ${port}`))
+app.listen(port, () => console.log(`Event Bus service listening ${port}`));
